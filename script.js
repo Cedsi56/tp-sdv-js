c9b5a74a6d8f90ca1bba7c7e5fae4c4693f898ca
Cedsi <cedsitails@gmail.com>
2022-01-21 15:00:17 +0100

    Header et Section

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 console.log("Test")
-const api_key = "7a29a0a7f45e106ee6a5aa46aba6feee440588b1";
+const API_URL = "https://www.giantbomb.com/api/games/";
+const API_KEY = "7a29a0a7f45e106ee6a5aa46aba6feee440588b1";
+const API_TO_JSON = "format=json";
